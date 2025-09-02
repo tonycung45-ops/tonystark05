c8361e33a78bccf44b467ae6b5de9d98e938b479
Tony Cung <tonycung45@gmail.com>
2025-09-02 11:33:46 +0700

    love the task

---
+++ github-assignment-tonycung.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	cout<<"Hello everyone my name is Tony Cung"<<endl;
+	cout<<"I curretly live in Jakarta"<<endl;
+	cout<<"I'm officially Binus University student"<<endl;
+	cout<<"Now I'm majoring in computer science which eager to learn more about technology and math"<<endl;
+	cout<<"I love using C language to learn programming"<<endl;
+	
+	return 0;
+	
+}
+
