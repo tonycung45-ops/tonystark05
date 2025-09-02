aaff1d733e0de143e9b40bea68dbfa7664cd1265
Tony Cung <tonycung45@gmail.com>
2025-09-02 10:56:54 +0700

    the third problem solver

---
+++ problem_c_onlinejudge_tony.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d\n", &a, &b);
+    printf("%d\n", a-b);
+
+    return 0;
+}
